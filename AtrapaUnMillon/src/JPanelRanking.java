ef35bcda9020e18653c677ea4b9b7b4550000645
G1T-J04KING <257858146+G1T-J04K1NG@users.noreply.github.com>
2026-05-12 20:20:14 +0200

    Trabajando en el Ranking

--- AtrapaUnMillon/src/JPanelRanking.java
+++ AtrapaUnMillon/src/JPanelRanking.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,6 +13,9 @@ import org.bson.Document;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class JPanelRanking extends JPanel {
 
@@ -19,7 +23,12 @@ public class JPanelRanking extends JPanel {
 	private JScrollPane PanelScrollDineroTotal;
 	private JScrollPane panelScrollMejorPartida;
 	private JTable TablaDineroTotal;
-
+	private JTable TablaDineroPartida;
+	private JLabel lblMejoresPuntuaciones;
+	private JLabel lblMáximosGanadores;
+	private ArrayList<Usuario> users;
+	private List<Usuario> ordenadosDineroUsuario,ordenadosDineroPartido;
+	
 	/**
 	 * Create the panel.
 	 */
@@ -28,11 +37,12 @@ public class JPanelRanking extends JPanel {
 		ArrayList<Usuario> users = new ArrayList<Usuario>();
 		rellenarUsuarios(users,collectionUsuarios);
 		
-		List<Usuario> ordenadosDineroUsuario = users.stream()
+		ordenadosDineroUsuario = users.stream()
 					.sorted(Comparator.comparingInt(Usuario::getDineroUsuario)).toList();
-		
-		List<Usuario> ordenadosDineroPartido = users.stream()
+		ordenadosDineroUsuario=ordenadosDineroUsuario.reversed();
+		ordenadosDineroPartido = users.stream()
 				.sorted(Comparator.comparingInt(Usuario::getDineroMejorPartida)).toList();
+		ordenadosDineroPartido=ordenadosDineroPartido.reversed();
 		
 		setBackground(Color.DARK_GRAY);
 		setLayout(null);
@@ -42,31 +52,158 @@ public class JPanelRanking extends JPanel {
 		PanelScrollDineroTotal.setBounds(150, 135, 300,500);
 		add(PanelScrollDineroTotal);
 		
+		TablaDineroPartida = new JTable();
+		TablaDineroPartida.getTableHeader().setReorderingAllowed(false);
+		TablaDineroPartida.getTableHeader().setResizingAllowed(false);
+		PanelScrollDineroTotal.setViewportView(TablaDineroPartida);
+		
+		DefaultTableModel modeloDineroPartida = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Posición", "Nombre", "DineroPartida"
+				}
+			);
+		modeloDineroPartida.setRowCount(0);
+		for (int i = 0 ; i < users.size() ; i++) {
+			Object[] fila = new Object[3];
+			fila[0] = i+1;
+			fila[1] = ordenadosDineroPartido.get(i).getNombre();
+			fila[2] = ordenadosDineroPartido.get(i).getDineroMejorPartida();
+			modeloDineroPartida.addRow(fila);
+		}
+		TablaDineroPartida.setModel(modeloDineroPartida);
+		
+		
+		
+		
 		panelScrollMejorPartida = new JScrollPane();
 		panelScrollMejorPartida.setBounds(750, 135, 300, 500);
 		add(panelScrollMejorPartida);
 		
 		TablaDineroTotal = new JTable();
-		DefaultTableModel model = new DefaultTableModel(
+		TablaDineroTotal.getTableHeader().setReorderingAllowed(false);
+		TablaDineroTotal.getTableHeader().setResizingAllowed(false);
+		
+		DefaultTableModel modeloDineroTotal = new DefaultTableModel(
 				new Object[][] {
 				},
 				new String[] {
 					"Posición", "Nombre", "Total Recaudado"
 				}
 			);
-		model.setRowCount(0);
+		modeloDineroTotal.setRowCount(0);
+		for (int i = 0 ; i < ordenadosDineroUsuario.size() ; i++) {
+			Object[] fila = new Object[3];
+			fila[0] = i+1;
+			fila[1] = ordenadosDineroUsuario.get(i).getNombre();
+			fila[2] = ordenadosDineroUsuario.get(i).getDineroUsuario();
+			modeloDineroTotal.addRow(fila);
+		}
+		TablaDineroTotal.setModel(modeloDineroTotal);
+		panelScrollMejorPartida.setViewportView(TablaDineroTotal);
+		
+		lblMejoresPuntuaciones = new JLabel("Mejores Puntuaciones Únicas");
+		lblMejoresPuntuaciones.setForeground(Color.WHITE);
+		lblMejoresPuntuaciones.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMejoresPuntuaciones.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMejoresPuntuaciones.setBounds(150, 58, 300, 67);
+		add(lblMejoresPuntuaciones);
+		
+		lblMáximosGanadores = new JLabel("Máximos Puntajes");
+		lblMáximosGanadores.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMáximosGanadores.setForeground(Color.WHITE);
+		lblMáximosGanadores.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMáximosGanadores.setBounds(750, 45, 300, 67);
+		add(lblMáximosGanadores);
+		
+		
+}
+	
+	
+	private void reiniciarSituacionInicial(MongoCollection<Document> collectionUsuarios) {
+		users = new ArrayList<Usuario>();
+		rellenarUsuarios(users,collectionUsuarios);
+		
+		ordenadosDineroUsuario = users.stream()
+					.sorted(Comparator.comparingInt(Usuario::getDineroUsuario)).toList();
+		ordenadosDineroUsuario=ordenadosDineroUsuario.reversed();
+		ordenadosDineroPartido = users.stream()
+				.sorted(Comparator.comparingInt(Usuario::getDineroMejorPartida)).toList();
+		ordenadosDineroPartido=ordenadosDineroPartido.reversed();
+		
+		setBackground(Color.DARK_GRAY);
+		setLayout(null);
+		setBounds(0, 30, 1200, 770);
+		
+		PanelScrollDineroTotal = new JScrollPane();
+		PanelScrollDineroTotal.setBounds(150, 135, 300,500);
+		add(PanelScrollDineroTotal);
+		
+		TablaDineroPartida = new JTable();
+		TablaDineroPartida.getTableHeader().setReorderingAllowed(false);
+		TablaDineroPartida.getTableHeader().setResizingAllowed(false);
+		PanelScrollDineroTotal.setViewportView(TablaDineroPartida);
+		
+		DefaultTableModel modeloDineroPartida = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Posición", "Nombre", "DineroPartida"
+				}
+			);
+		modeloDineroPartida.setRowCount(0);
 		for (int i = 0 ; i < users.size() ; i++) {
 			Object[] fila = new Object[3];
 			fila[0] = i+1;
-			fila[1] = users.get(i).getNombre();
-			fila[2] = users.get(i).getDineroUsuario();
-			model.addRow(fila);
+			fila[1] = ordenadosDineroPartido.get(i).getNombre();
+			fila[2] = ordenadosDineroPartido.get(i).getDineroMejorPartida();
+			modeloDineroPartida.addRow(fila);
 		}
-		TablaDineroTotal.setModel(model);
+		TablaDineroPartida.setModel(modeloDineroPartida);
+		
+		
+		
+		
+		panelScrollMejorPartida = new JScrollPane();
+		panelScrollMejorPartida.setBounds(750, 135, 300, 500);
+		add(panelScrollMejorPartida);
+		
+		TablaDineroTotal = new JTable();
+		TablaDineroTotal.getTableHeader().setReorderingAllowed(false);
+		TablaDineroTotal.getTableHeader().setResizingAllowed(false);
+		
+		DefaultTableModel modeloDineroTotal = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Posición", "Nombre", "Total Recaudado"
+				}
+			);
+		modeloDineroTotal.setRowCount(0);
+		for (int i = 0 ; i < ordenadosDineroUsuario.size() ; i++) {
+			Object[] fila = new Object[3];
+			fila[0] = i+1;
+			fila[1] = ordenadosDineroUsuario.get(i).getNombre();
+			fila[2] = ordenadosDineroUsuario.get(i).getDineroUsuario();
+			modeloDineroTotal.addRow(fila);
+		}
+		TablaDineroTotal.setModel(modeloDineroTotal);
 		panelScrollMejorPartida.setViewportView(TablaDineroTotal);
 		
+		lblMejoresPuntuaciones = new JLabel("Mejores Puntuaciones Únicas");
+		lblMejoresPuntuaciones.setForeground(Color.WHITE);
+		lblMejoresPuntuaciones.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMejoresPuntuaciones.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMejoresPuntuaciones.setBounds(150, 58, 300, 67);
+		add(lblMejoresPuntuaciones);
 		
-
+		lblMáximosGanadores = new JLabel("Máximos Puntajes");
+		lblMáximosGanadores.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMáximosGanadores.setForeground(Color.WHITE);
+		lblMáximosGanadores.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblMáximosGanadores.setBounds(750, 45, 300, 67);
+		add(lblMáximosGanadores);
 	}
 
 	private void rellenarUsuarios(ArrayList<Usuario> users,MongoCollection<Document> collectionUsuarios) {
@@ -79,4 +216,7 @@ public class JPanelRanking extends JPanel {
 		}
 		
 	}
+	
+	
+	
 }
--- AtrapaUnMillon/src/MainApp.java
+++ AtrapaUnMillon/src/MainApp.java
@@ -11,15 +11,7 @@ public class MainApp {
 	public static void main (String [] args) {
 		
 		 try  { 
-			 	//Conexión a la BDD
-			 	String url = "mongodb+srv://joaest22_db_user:"
-			 				+ "UnjWo1ilrimUXFVL@atrapa1millon.dli87oo.mongodb.net/?appName=Atrapa1Millon";
-	        	MongoClient mongoClient = MongoClients.create(url);
-	            
-	        	MongoDatabase database = mongoClient.getDatabase("atrapa1millon");
-	            MongoCollection<Document> collectionUsuarios = database.getCollection("usuarios");
-	            MongoCollection<Document> collectionPreguntas = database.getCollection("preguntas");
-	            
+			 	
 	            
 	            //InciarJframeInterfaz
 	            
