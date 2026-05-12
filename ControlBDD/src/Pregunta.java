19fd63a38a6fcc9c4785e804777e27975cd6abb0
Javier Puche <javier15000@gmail.com>
2026-05-12 13:55:40 +0200

    He hecho un proyecto aparte para gestionar la base de datos y añadimos las preguntas

---
+++ ControlBDD/src/Pregunta.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Pregunta {
+
+	//ATRIBUTOS
+	
+	private String pregunta;
+	private String respuestaCorrecta;
+	private ArrayList<String> respuestas;
+	private int dificultad;
+	
+	
+	public Pregunta(String pregunta, String respuestaCorrecta, ArrayList<String> respuestas, int dificultad) {
+		this.pregunta = pregunta;
+		this.respuestaCorrecta = respuestaCorrecta;
+		this.respuestas = respuestas;
+		this.dificultad = dificultad;
+	}
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	public String getRespuestaCorrecta() {
+		return respuestaCorrecta;
+	}
+	public void setRespuestaCorrecta(String respuestaCorrecta) {
+		this.respuestaCorrecta = respuestaCorrecta;
+	}
+	public ArrayList<String> getRespuestas() {
+		return respuestas;
+	}
+	public void setRespuestas(ArrayList<String> respuestas) {
+		this.respuestas = respuestas;
+	}
+	public int getDificultad() {
+		return dificultad;
+	}
+	public void setDificultad(int dificultad) {
+		this.dificultad = dificultad;
+	}
+}
+
+
